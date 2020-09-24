b67da618633cedb88096b9035d9eee17529a3776
J. Alvarez <jcalvarezj@gmail.com>
2020-09-24 15:34:23 -0500

    Solve challenge

--- challenge.py
+++ challenge.py
@@ -1,8 +1,11 @@
 import time
 
 
-def finish_date():
-    # You have to code here!!
+def finish_date(func):
+    def internal_func(*args, **kwargs):
+        func(*args, **kwargs)
+        print(f'The function {func.__name__} finished at {time.strftime("%d/%m/%Y - %H:%M:%S", time.gmtime())}')
+    return internal_func
 
 
 @finish_date
